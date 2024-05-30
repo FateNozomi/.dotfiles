c20c889dd58924f8266efdb3807cac79191956c6
FateNozomi <wailinnyu@gmail.com>
2024-05-30 23:58:19 +0800

    Add pwsh install script

---
+++ install.ps1
@@ -0,0 +1,2 @@
+New-Item -Path $HOME\.vimrc -ItemType SymbolicLink -Value $HOME\.dotfiles\.vimrc
+New-Item -Path $PROFILE -ItemType SymbolicLink -Value $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
