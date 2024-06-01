a11997c18bd2bb9bcf8ff24ac7a61e088c08a3dc
FateNozomi <wailinnyu@gmail.com>
2024-06-01 21:46:57 +0800

    Add vscode setting.json

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 New-Item -Path $HOME\.vimrc -ItemType SymbolicLink -Value $HOME\.dotfiles\.vimrc
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
+New-Item -Path $env:APPDATA\Code\User\settings.json -ItemType SymbolicLink -Value $HOME\.dotfiles\vscode\settings.json
 
 New-Item -Path $HOME\vimfiles\pack -ItemType SymbolicLink -Value $HOME\.vim\pack
