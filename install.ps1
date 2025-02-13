57fedf707bd2adcfe264d22eaaef091f9c16f505
FateNozomi <wailinnyu@gmail.com>
2025-02-14 00:15:17 +0800

    Add nvim init.lua

--- install.ps1
+++ install.ps1
@@ -2,6 +2,9 @@
 New-Item -Path $HOME\.vimrc -ItemType SymbolicLink -Value $HOME\.dotfiles\.vimrc
 New-Item -Path $HOME\vimfiles\pack -ItemType SymbolicLink -Value $HOME\.vim\pack
 
+# nvim
+New-Item -Path $env:LOCALAPPDATA\nvim\init.lua -ItemType SymbolicLink -Value $HOME\.dotfiles\nvim\init.lua
+
 # pwsh profile
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
 
