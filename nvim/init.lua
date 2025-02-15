0d93443e94b948248776422a7da40baa64f7bf68
FateNozomi <wailinnyu@gmail.com>
2025-02-15 15:47:46 +0800

    Format nvim <CMD>

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ require("config.lazy")
 vim.cmd.colorscheme "catppuccin"
 
 if vim.g.vscode then
-  vim.keymap.set('n', 'gd', "<cmd>lua require('vscode').action('editor.action.goToImplementation')<CR>")
-  vim.keymap.set('n', 'gr', "<cmd>lua require('vscode').action('editor.action.goToReferences')<CR>")
-  vim.keymap.set('n', '<leader>f', "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
+  vim.keymap.set('n', 'gd', "<CMD>lua require('vscode').action('editor.action.goToImplementation')<CR>")
+  vim.keymap.set('n', 'gr', "<CMD>lua require('vscode').action('editor.action.goToReferences')<CR>")
+  vim.keymap.set('n', '<leader>f', "<CMD>lua require('vscode').action('editor.action.formatDocument')<CR>")
 end
