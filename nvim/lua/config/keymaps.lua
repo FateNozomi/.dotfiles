bbf8c38ddfc9b59e51d0206a8612130a5f64d264
FateNozomi <wailinnyu@gmail.com>
2025-02-16 19:34:53 +0800

    Add nvim lspconfig and blink-cmp plugins

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,7 +10,18 @@ map('n', '<leader>w', '<CMD>w<CR>')
 -- Copy to system clipboard
 map('n', '<leader>y', '"+Y')
 map('v', '<leader>y', '"+y')
+
+-- Paste from system clipboard
 map('n', '<leader>p', '"+p')
+map('v', '<leader>p', '"+p')
+
+-- LSP
+map('n', 'grr', vim.lsp.buf.rename, { desc = 'Renames all references' })
+map('n', 'gra', vim.lsp.buf.code_action, { desc = 'Code action' })
+map('n', 'grn', vim.lsp.buf.references, { desc = 'Lists all the references' })
+map('n', 'gri', vim.lsp.buf.implementation, { desc = 'Lists all the implementations' })
+map('n', 'gO', vim.lsp.buf.document_symbol, { desc = 'Lists all symbols in the current buffer' })
+map('i', '<C-s>', vim.lsp.buf.signature_help, { desc = 'Display signature information' })
 
 -- Oil
 map('n', '<leader>-', '<CMD>Oil<CR>')
