e87aa9f8d7a6165487f70e6b9df47c7d63c1cd55
William Liew <william.liew@belldirect.com.au>
2025-02-17 17:32:51 +1100

    Update nvim lsp keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,10 +16,12 @@ map('n', '<leader>p', '"+p')
 map('v', '<leader>p', '"+p')
 
 -- LSP
-map('n', 'grr', vim.lsp.buf.rename, { desc = 'Renames all references' })
-map('n', 'gra', vim.lsp.buf.code_action, { desc = 'Code action' })
-map('n', 'grn', vim.lsp.buf.references, { desc = 'Lists all the references' })
-map('n', 'gri', vim.lsp.buf.implementation, { desc = 'Lists all the implementations' })
+map('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to Definition' })
+map('n', 'gr', vim.lsp.buf.references, { desc = 'Find All References' })
+map('n', 'gI', vim.lsp.buf.implementation, { desc = 'Find All Implementations' })
+map('n', 'gA', vim.lsp.buf.code_action, { desc = 'Code action' })
+map('n', 'gR', vim.lsp.buf.rename, { desc = 'Rename Symbol' })
+
 map('n', 'gO', vim.lsp.buf.document_symbol, { desc = 'Lists all symbols in the current buffer' })
 map('i', '<C-s>', vim.lsp.buf.signature_help, { desc = 'Display signature information' })
 map('n', '<leader>f', vim.lsp.buf.format, { desc = 'Formats buffer' })
@@ -29,3 +31,8 @@ map('n', '<leader>-', '<CMD>Oil<CR>')
 
 -- FzfLua
 map('n', '<C-p>', '<CMD>FzfLua files<CR>')
+map('n', '<leader>f?', '<CMD>FzfLua<CR>', { desc = 'FzfLua Builtin' })
+
+map('n', '<leader>la', '<CMD>FzfLua lsp_code_actions<CR>', { desc = 'Code action' })
+map('n', '<leader>lr', '<CMD>FzfLua lsp_references<CR>', { desc = 'Find All References' })
+map('n', '<leader>lI', '<CMD>FzfLua lsp_implementations<CR>', { desc = 'Find All Implementations' })
