42958a2e5bfbbedeac261a5d8dcd7804edfb05c2
William Liew <william.liew@belldirect.com.au>
2025-02-17 13:03:17 +1100

    Add nvim disable plugins when not vim.g.vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,10 @@ require("config.settings")
 require("config.lazy")
 require("config.keymaps")
 
-vim.cmd.colorscheme "catppuccin"
 
-if vim.g.vscode then
+if not vim.g.vscode then
+  vim.cmd.colorscheme "catppuccin"
+else
   vim.keymap.set('n', 'gd', "<CMD>lua require('vscode').action('editor.action.goToImplementation')<CR>")
   vim.keymap.set('n', 'gr', "<CMD>lua require('vscode').action('editor.action.goToReferences')<CR>")
   vim.keymap.set('n', '<leader>f', "<CMD>lua require('vscode').action('editor.action.formatDocument')<CR>")
