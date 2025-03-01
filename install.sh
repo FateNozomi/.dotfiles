2234d27bb18925274a1b6b2b1fe1804e039bb900
FateNozomi <wailinnyu@gmail.com>
2025-03-01 15:24:01 +0800

    Add install.sh for wsl

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# neovim
+ln -s ~/.src/.dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/.src/.dotfiles/nvim/after/ ~/.config/nvim/
+ln -s ~/.src/.dotfiles/nvim/lua/ ~/.config/nvim/
