ac9834bc171f19d859edab6608becebb8e1f23ab
William Liew <william.liew@belldirect.com.au>
2025-03-05 03:30:06 +1100

    Add nvim conform plugin

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -17,41 +17,23 @@ return {
     },
     opts = {
       servers = {
-        lua_ls = {}
-      }
+        lua_ls = {},
+      },
     },
     config = function(_, opts)
-      local lspconfig = require('lspconfig')
+      local lspconfig = require("lspconfig")
       for server, config in pairs(opts.servers) do
         -- passing config.capabilities to blink.cmp merges with the capabilities in your
         -- `opts[server].capabilities, if you've defined it
-        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
         lspconfig[server].setup(config)
       end
 
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
+      vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
         callback = function()
           vim.lsp.codelens.refresh({ bufnr = 0 })
-        end
+        end,
       })
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then
-            return
-          end
-
-          if client.supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end
-            })
-          end
-        end
-      })
-    end
-  }
+    end,
+  },
 }
