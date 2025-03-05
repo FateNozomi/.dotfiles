1f548764a2764b49510c276a6902de9f271a51dc
William Liew <william.liew@belldirect.com.au>
2025-03-06 00:04:38 +1100

    Set nvim fzf-lua preview to vertical

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -3,5 +3,12 @@ return {
     "ibhagwan/fzf-lua",
     dependencies = { "echasnovski/mini.icons" },
     opts = {},
+    config = function()
+      require("fzf-lua").setup({
+        winopts = {
+          preview = { layout = "vertical" },
+        },
+      })
+    end,
   },
 }
