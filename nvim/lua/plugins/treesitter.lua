a83bbf8dbdf04941aa41acd0cec5502ed14b1faf
William Liew <william.liew@belldirect.com.au>
2025-03-07 12:21:36 +1100

    Set nvim foldexpr to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -11,9 +11,13 @@ return {
           enable = true,
         },
         indent = {
-          enable = true,
+          enable = false,
         },
       })
+
+      vim.wo.foldmethod = "expr"
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.wo.foldlevel = 99
     end,
   },
 }
