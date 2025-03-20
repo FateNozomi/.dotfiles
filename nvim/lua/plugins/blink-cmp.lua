90277615a0a66f4009df35c6a21d46ef43baa95a
William Liew <william.liew@belldirect.com.au>
2025-03-20 11:56:16 +1100

    Update nvim blink-cmp config

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -2,7 +2,7 @@ return {
   {
     "saghen/blink.cmp",
     -- optional: provides snippets for the snippet source
-    dependencies = "rafamadriz/friendly-snippets",
+    dependencies = { "rafamadriz/friendly-snippets" },
 
     -- use a release tag to download pre-built binaries
     version = "*",
@@ -27,11 +27,7 @@ return {
       keymap = { preset = "super-tab" },
 
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
@@ -42,7 +38,7 @@ return {
         default = { "lsp", "path", "snippets", "buffer" },
       },
 
-      -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
       -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
       -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
       --
