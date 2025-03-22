3d21ca29ed93d087cbe894eabf474719f38c6225
FateNozomi <wailinnyu@gmail.com>
2025-03-22 09:12:38 +0800

    Add set openssh default shell script

--- install.ps1
+++ install.ps1
@@ -1,3 +1,9 @@
+# pwsh profile
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
+
+# wezterm
+New-Item -ItemType SymbolicLink -Path $HOME\.config\wezterm -Target $HOME\.dotfiles\wezterm
+
 # vim
 New-Item -ItemType SymbolicLink -Path $HOME\.vimrc -Target $HOME\.dotfiles\.vimrc
 New-Item -ItemType SymbolicLink -Path $HOME\vimfiles\autoload -Target $HOME\.vim\autoload
@@ -6,12 +12,6 @@ New-Item -ItemType SymbolicLink -Path $HOME\vimfiles\autoload -Target $HOME\.vim
 New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim\init.lua -Target $HOME\.dotfiles\nvim\init.lua
 New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim\lua -Target $HOME\.dotfiles\nvim\lua
 New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim\after -Target $HOME\.dotfiles\nvim\after
-
-# wezterm
-New-Item -ItemType SymbolicLink -Path $HOME\.config\wezterm -Target $HOME\.dotfiles\wezterm
-
-# pwsh profile
-New-Item -ItemType SymbolicLink -Path $PROFILE -Target $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
 
 # vscode
 New-Item -ItemType SymbolicLink -Path $env:APPDATA\Code\User\settings.json -Target $HOME\.dotfiles\vscode\settings.json
