c4a4d73b96ef81453ad61343592ebf4a6de1cf28
William Liew <william.liew@belldirect.com.au>
2025-03-25 02:11:44 +1100

    Change nvim cs ftplugin errorfomat to lua

--- nvim/after/ftplugin/cs.lua
+++ nvim/after/ftplugin/cs.lua
@@ -1,6 +1,3 @@
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
-
-vim.cmd([[set errorformat=%E%f(%l\\,%c):\ %trror\ %m\ [%.%#],
-                         \%W%f(%l\\,%c):\ %tarning\ %m\ [%.%#],
-                         \%-G%.%#]])
+vim.opt_local.errorformat = "%E%f(%l\\,%c): %trror %m,%W%f(%l\\,%c): %tarning %m,%-G%.%#"
