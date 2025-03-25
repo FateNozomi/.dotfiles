af836a014cbcdae3d25e94787aa2ddf5ac7324a8
William Liew <william.liew@belldirect.com.au>
2025-03-26 00:06:21 +1100

    Improve openssh-default-shell.ps1 script

--- openssh-default-shell.ps1
+++ openssh-default-shell.ps1
@@ -1,1 +1,34 @@
-New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\WSL\wsl.exe" -PropertyType String -Force
+<#
+.SYNOPSIS
+    Sets the default shell for OpenSSH.
+.DESCRIPTION
+    This PowerShell script sets the default shell for OpenSSH.
+.PARAMETER Shell
+    Specifies the default shell. [pwsh|wsl]
+.EXAMPLE
+    PS> ./openssh-default-shell.ps1 -Shell pwsh
+#>
+
+#Requires -RunAsAdministrator
+
+param(
+    [String]$Shell
+)
+
+if (([string]::IsNullOrEmpty($Shell))) {
+    $Shell = Read-Host "Enter OpenSSH default shell [pwsh|wsl]"
+    if (([string]::IsNullOrEmpty($Shell))) {
+        exit 0
+    }
+}
+
+if ($Shell -eq "pwsh") {
+    New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\PowerShell\7\pwsh.exe" -PropertyType String -Force
+}
+elseif ($Shell -eq "wsl") {
+    New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\WSL\wsl.exe" -PropertyType String -Force
+}
+else {
+    Write-Host "Invalid shell specified."
+    exit 1
+}
