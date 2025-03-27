57603020f0185edce9ac511364f95b51756a68a8
William Liew <william.liew@belldirect.com.au>
2025-03-27 16:25:11 +1100

    Move nvim diagnostic config to lsp.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,10 +5,6 @@ require("config.keymaps")
 vim.opt.title = true
 vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
 
-vim.diagnostic.config({
-  virtual_lines = { current_line = true },
-})
-
 if not vim.g.vscode then
   vim.cmd.colorscheme("kanagawa")
   require("config.lsp")
--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -6,3 +6,7 @@ vim.lsp.enable({
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
   callback = function() vim.lsp.codelens.refresh({ bufnr = 0 }) end,
 })
+
+vim.diagnostic.config({
+  virtual_lines = { current_line = true },
+})
