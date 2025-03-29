40c9f6cabc085605db759946fd4785fb8e685b0c
FateNozomi <wailinnyu@gmail.com>
2025-03-29 23:49:23 +0800

    Clean up nvim plugins move setup to opts

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,18 +7,15 @@ return {
       local harpoon = require("harpoon")
       harpoon:setup()
 
-      vim.keymap.set("n", "<leader>hh", function()
-        harpoon:list():add()
-      end, { desc = "Add to Harpoon" })
-      vim.keymap.set("n", "<C-h>", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end, { desc = "List Harpoon" })
-      vim.keymap.set("n", "<leader>hn", function()
-        harpoon:list():prev()
-      end, { desc = "Jump to the previous Harpoon" })
-      vim.keymap.set("n", "<leader>hp", function()
-        harpoon:list():next()
-      end, { desc = "Jump to the next Harpoon" })
+      vim.keymap.set("n", "<leader>hh", function() harpoon:list():add() end, { desc = "Add to Harpoon" })
+      vim.keymap.set(
+        "n",
+        "<C-h>",
+        function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+        { desc = "List Harpoon" }
+      )
+      vim.keymap.set("n", "<leader>hn", function() harpoon:list():prev() end, { desc = "Jump to the previous Harpoon" })
+      vim.keymap.set("n", "<leader>hp", function() harpoon:list():next() end, { desc = "Jump to the next Harpoon" })
     end,
   },
 }
