6c078b2db325b45783d00f56f1f7eaca134693d3
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:06 +1100

    Add mpv.conf

--- install.ps1
+++ install.ps1
@@ -1,5 +1,14 @@
+# vim
 New-Item -Path $HOME\.vimrc -ItemType SymbolicLink -Value $HOME\.dotfiles\.vimrc
+New-Item -Path $HOME\vimfiles\pack -ItemType SymbolicLink -Value $HOME\.vim\pack
+
+# pwsh profile
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
+
+# vscode
 New-Item -Path $env:APPDATA\Code\User\settings.json -ItemType SymbolicLink -Value $HOME\.dotfiles\vscode\settings.json
 
-New-Item -Path $HOME\vimfiles\pack -ItemType SymbolicLink -Value $HOME\.vim\pack
+# mpv
+New-Item -Path $env:APPDATA\mpv\mpv.conf -ItemType SymbolicLink -Value $HOME\.dotfiles\mpv\mpv.conf
+$PICTURES = [environment]::getfolderpath("mypictures")
+New-Item -Path $env:APPDATA\mpv\screenshots -ItemType SymbolicLink -Value $PICTURES\screenshots
