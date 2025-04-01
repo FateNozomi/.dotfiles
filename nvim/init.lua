bf53df14ab6843cd49b6efbc5d03928973a1bf78
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Set nvim cwd as title

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,9 @@ require("config.settings")
 require("config.lazy")
 require("config.keymaps")
 
+vim.opt.title = true
+vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
+
 if not vim.g.vscode then
   vim.cmd.colorscheme("kanagawa")
 end
