780629c5b7534ccb999109cd4f8089f93c92ccc8
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Switch nvim to buildin LSP setup and configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,11 @@ require("config.keymaps")
 vim.opt.title = true
 vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
 
+vim.diagnostic.config({
+  virtual_lines = { current_line = true },
+})
+
 if not vim.g.vscode then
   vim.cmd.colorscheme("kanagawa")
+  require("config.lsp")
 end
