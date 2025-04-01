9b560a5e82dcae5638c4bed454659d65a15065a8
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Clean up symbolic links

--- install.ps1
+++ install.ps1
@@ -1,22 +1,20 @@
 # vim
-New-Item -Path $HOME\.vimrc -ItemType SymbolicLink -Value $HOME\.dotfiles\.vimrc
-New-Item -Path $HOME\vimfiles\pack -ItemType SymbolicLink -Value $HOME\.vim\pack
+New-Item -ItemType SymbolicLink -Path $HOME\.vimrc -Target $HOME\.dotfiles\.vimrc
+New-Item -ItemType SymbolicLink -Path $HOME\vimfiles\pack -Target $HOME\.vim\pack
 
 # nvim
-New-Item -Path $env:LOCALAPPDATA\nvim\init.lua -ItemType SymbolicLink -Value $HOME\.dotfiles\nvim\init.lua
-New-Item -Path $env:LOCALAPPDATA\nvim\lua\config\settings.lua -ItemType SymbolicLink -Value $HOME\.dotfiles\nvim\config\settings.lua
-New-Item -Path $env:LOCALAPPDATA\nvim\lua\config\lazy.lua -ItemType SymbolicLink -Value $HOME\.dotfiles\nvim\config\lazy.lua
-New-Item -Path $env:LOCALAPPDATA\nvim\lua\plugins\colorscheme.lua -ItemType SymbolicLink -Value $HOME\.dotfiles\nvim\plugins\colorscheme.lua
+New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim\init.lua -Target $HOME\.dotfiles\nvim\init.lua
+New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim\lua -Target $HOME\.dotfiles\nvim\lua
 
 # pwsh profile
-New-Item -Path $PROFILE -ItemType SymbolicLink -Value $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
 
 # vscode
-New-Item -Path $env:APPDATA\Code\User\settings.json -ItemType SymbolicLink -Value $HOME\.dotfiles\vscode\settings.json
-New-Item -Path $env:APPDATA\Code\User\keybindings.json -ItemType SymbolicLink -Value $HOME\.dotfiles\vscode\keybindings.json
+New-Item -ItemType SymbolicLink -Path $env:APPDATA\Code\User\settings.json -Target $HOME\.dotfiles\vscode\settings.json
+New-Item -ItemType SymbolicLink -Path $env:APPDATA\Code\User\keybindings.json -Target $HOME\.dotfiles\vscode\keybindings.json
 
 # mpv
-New-Item -Path $env:APPDATA\mpv\mpv.conf -ItemType SymbolicLink -Value $HOME\.dotfiles\mpv\mpv.conf
+New-Item -ItemType SymbolicLink -Path $env:APPDATA\mpv\mpv.conf -Target $HOME\.dotfiles\mpv\mpv.conf
 $PICTURES = [environment]::getfolderpath("mypictures")
-New-Item -Path $env:APPDATA\mpv\screenshots -ItemType SymbolicLink -Value $PICTURES\screenshots
+New-Item -ItemType SymbolicLink -Path $env:APPDATA\mpv\screenshots -Target $PICTURES\screenshots
 
