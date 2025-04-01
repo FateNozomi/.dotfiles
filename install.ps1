5bcf69876155247f205d019849b3f2bf49e0d73b
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:06 +1100

    Add symlink for ~/.vim/pack to ~/vimfiles/pack

--- install.ps1
+++ install.ps1
@@ -1,2 +1,4 @@
 New-Item -Path $HOME\.vimrc -ItemType SymbolicLink -Value $HOME\.dotfiles\.vimrc
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value $HOME\.dotfiles\Microsoft.PowerShell_profile.ps1
+
+New-Item -Path $HOME\vimfiles\pack -ItemType SymbolicLink -Value $HOME\.vim\pack
