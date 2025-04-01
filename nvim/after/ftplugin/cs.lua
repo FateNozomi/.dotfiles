112b5811766eb26c58efbf1e99275a6527140714
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Add nvim ftplugin errorformat for dotnet build

--- nvim/after/ftplugin/cs.lua
+++ nvim/after/ftplugin/cs.lua
@@ -1,2 +1,6 @@
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
+
+vim.cmd([[set errorformat=%E%f(%l\\,%c):\ %trror\ %m\ [%.%#],
+                         \%W%f(%l\\,%c):\ %tarning\ %m\ [%.%#],
+                         \%-G%.%#]])
