539ae8dd8635bfec2ce23817f3e20f7bbaf066de
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Add nvim gitsigns on_attach keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,36 @@
 return {
   "lewis6991/gitsigns.nvim",
-  event = "BufReadPre",
-  config = function() require("gitsigns").setup() end,
+  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+  opts = {
+    on_attach = function(bufnr)
+      local gs = require("gitsigns")
+
+      local function map(mode, l, r, desc) vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc }) end
+
+      map("n", "]c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
+        end
+      end)
+
+      map("n", "[c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
+        end
+      end)
+
+      map({ "n", "v" }, "<leader>ghs", gs.stage_hunk, "Stage Hunk")
+      map({ "n", "v" }, "<leader>ghr", gs.reset_hunk, "Reset Hunk")
+      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>ghB", gs.blame, "Blame Buffer")
+      map("n", "<leader>ghd", gs.diffthis, "Diff This")
+    end,
+  },
 }
