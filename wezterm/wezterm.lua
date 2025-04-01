3dd25b15e27bf1d2fe87a1d9b210758218a50d74
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Add wezterm.lua config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,45 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Kanagawa (Gogh)"
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+config.font_size = 11.5
+
+config.enable_scroll_bar = true
+config.default_domain = "WSL:Ubuntu"
+
+-- Keymaps
+config.keys = {
+  {
+    mods = "SHIFT|ALT",
+    key = "_",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    mods = "SHIFT|ALT",
+    key = "+",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    mods = "ALT",
+    key = "j",
+    action = wezterm.action.ActivatePaneDirection("Down"),
+  },
+  {
+    mods = "ALT",
+    key = "k",
+    action = wezterm.action.ActivatePaneDirection("Up"),
+  },
+  {
+    mods = "ALT",
+    key = "h",
+    action = wezterm.action.ActivatePaneDirection("Left"),
+  },
+  {
+    mods = "ALT",
+    key = "l",
+    action = wezterm.action.ActivatePaneDirection("Right"),
+  },
+}
+
+return config
