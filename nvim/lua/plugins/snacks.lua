554e5af41fed354d04d80cb64aa6c5742ccf0d1d
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Add nvim snacks lazygit keybind

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -80,6 +80,7 @@ return {
       { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
       { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
       -- Other
+      { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
       { "<leader>ft", function() Snacks.terminal() end, desc = "Toggle Terminal" },
     },
   },
