828ed249c66dea468ffb5cd89c385d98eb28a63b
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Add nvim format keymap and on save

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -35,9 +35,27 @@ return {
         lspconfig[server].setup(config)
       end
 
-      vim.api.nvim_create_autocmd({"BufEnter", "CursorHold", "InsertLeave"}, {
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
         callback = function()
           vim.lsp.codelens.refresh({ bufnr = 0 })
+        end
+      })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then
+            return
+          end
+
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end
+            })
+          end
         end
       })
     end
