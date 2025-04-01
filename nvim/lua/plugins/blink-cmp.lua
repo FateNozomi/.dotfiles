c4cb8a8bd7474e4f7b9ba75734351f68f6bb04e4
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Update nvim blink-cmp config

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -5,7 +5,7 @@ return {
     dependencies = { "rafamadriz/friendly-snippets" },
 
     -- use a release tag to download pre-built binaries
-    version = "*",
+    version = "1.*",
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:
@@ -14,16 +14,18 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
       --
       -- All presets have the following mappings:
       -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
       -- C-e: Hide menu
-      -- C-k: Toggle signature help
+      -- C-k: Toggle signature help (if signature.enabled = true)
       --
-      -- See the full "keymap" documentation for information on defining your own keymap.
+      -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = { preset = "super-tab" },
 
       appearance = {
@@ -31,6 +33,9 @@ return {
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
