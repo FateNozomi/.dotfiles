d6b13247ac06a1a94f8ff5a611a42d10a65141ab
FateNozomi <wailinnyu@gmail.com>
2025-04-02 10:46:07 +1100

    Use neovide config file to set font

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,3 @@ vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
 if not vim.g.vscode then
   vim.cmd.colorscheme("kanagawa")
 end
-
-if vim.g.neovide then
-  vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h12"
-end
