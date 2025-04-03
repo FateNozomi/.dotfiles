0419375aa1f06fed2d260c25868574377495bc5e
FateNozomi <wailinnyu@gmail.com>
2025-04-03 20:07:22 +0800

    Clean up keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -23,33 +23,17 @@ map("n", "<leader>p", '"+p', { desc = 'Paste text from "+ after the cursor' })
 map("n", "<leader>P", '"+P', { desc = 'Paste text from "+ before the cursor' })
 map("v", "<leader>p", '"+p', { desc = 'Replace text from "+ ' })
 
-if not vim.g.vscode then
-  map("t", "<Esc>", "<C-\\><C-n>", { noremap = true, desc = "Terminal normal mode" })
-  map("t", "<M-r>", [['<C-\><C-N>"'.nr2char(getchar()).'pi']], { expr = true })
-else
-  map(
-    "n",
-    "<leader>ft",
-    "<CMD>lua require('vscode').action('workbench.action.terminal.focus')<CR>",
-    { desc = "Terminal" }
-  )
-end
+-- Terminal
+map("t", "<Esc>", "<C-\\><C-n>", { noremap = true, desc = "Terminal normal mode" })
+map("t", "<M-r>", [['<C-\><C-N>"'.nr2char(getchar()).'pi']], { expr = true })
 
 -- LSP
-if not vim.g.vscode then
-  map("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
-  map("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
-  map("n", "gr", vim.lsp.buf.references, { desc = "References" })
-  map("n", "gI", vim.lsp.buf.implementation, { desc = "Goto Implementations" })
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
-  map("n", "<leader>fd", vim.lsp.buf.format, { desc = "Format Buffer" })
-else
-  map("n", "gd", "<CMD>lua require('vscode').action('editor.action.revealDefinition')<CR>")
-  map("n", "gr", "<CMD>lua require('vscode').action('editor.action.goToReferences')<CR>")
-  map("n", "gI", "<CMD>lua require('vscode').action('editor.action.goToImplementation')<CR>")
-  map({ "n", "v" }, "<leader>ca", "<CMD>lua require('vscode').action('editor.action.codeAction')<CR>")
-  map("n", "<leader>fd", "<CMD>lua require('vscode').action('editor.action.formatDocument')<CR>")
-end
+map("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+map("n", "gr", vim.lsp.buf.references, { desc = "References" })
+map("n", "gI", vim.lsp.buf.implementation, { desc = "Goto Implementations" })
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+map("n", "<leader>fd", vim.lsp.buf.format, { desc = "Format Buffer" })
 
 map("n", "gR", vim.lsp.buf.rename, { desc = "Rename Symbol" })
 map("n", "gO", vim.lsp.buf.document_symbol, { desc = "Lists all symbols in the current buffer" })
@@ -61,33 +45,33 @@ map("n", "<leader>xl", vim.diagnostic.setloclist, { desc = "Location List" })
 -- quickfix list
 map("n", "<leader>xq", vim.diagnostic.setqflist, { desc = "Quickfix List" })
 
-map("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
-map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
-
 -- diagnostic
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 
--- Oil
-if not vim.g.vscode then
-  map("n", "<leader>-", "<CMD>Oil<CR>")
-else
+if vim.g.vscode then
+  -- vscode Terminal
+  map(
+    "n",
+    "<leader>ft",
+    "<CMD>lua require('vscode').action('workbench.action.terminal.focus')<CR>",
+    { desc = "Terminal" }
+  )
+
+  -- vscode LSP
+  map("n", "gd", "<CMD>lua require('vscode').action('editor.action.revealDefinition')<CR>")
+  map("n", "gD", "<CMD>lua require('vscode').action('editor.action.revealDeclaration')<CR>")
+  map("n", "gr", "<CMD>lua require('vscode').action('editor.action.goToReferences')<CR>")
+  map("n", "gI", "<CMD>lua require('vscode').action('editor.action.goToImplementation')<CR>")
+  map({ "n", "v" }, "<leader>ca", "<CMD>lua require('vscode').action('editor.action.codeAction')<CR>")
+  map("n", "<leader>fd", "<CMD>lua require('vscode').action('editor.action.formatDocument')<CR>")
+
+  -- vscode Explorer
   map("n", "-", "<CMD>lua require('vscode').action('workbench.view.explorer')<CR>")
-end
 
--- vscode Harpoon
-if vim.g.vscode then
-  map("n", "<leader>hh", "<CMD>lua require('vscode').action('harpoon-vscode.addToHarpoon')<CR>")
-  map("n", "<C-h>", "<CMD>lua require('vscode').action('harpoon-vscode.goToHarpoon')<CR>")
-  map("n", "<leader>he", "<CMD>lua require('vscode').action('harpoon-vscode.editHarpoon')<CR>")
-end
-
--- vscode Bookmarks
-if vim.g.vscode then
+  -- vscode Bookmarks
   map("n", "<leader>mm", "<CMD>lua require('vscode').action('bookmarks.toggle')<CR>")
   map("n", "<leader>ml", "<CMD>lua require('vscode').action('bookmarks.listFromAllFiles')<CR>")
-end
 
--- vscode Centered Layout
-if vim.g.vscode then
+  -- vscode Centered Layout
   map("n", "<leader>z", "<CMD>lua require('vscode').action('workbench.action.toggleCenteredLayout')<CR>")
 end
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,10 +8,10 @@ return {
         ["cd"] = { "actions.cd", mode = "n" },
       },
     },
-    -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
+    keys = {
+      { "<leader>-", "<CMD>Oil<CR>", desc = "Oil Explorer" },
+    },
   },
 }
