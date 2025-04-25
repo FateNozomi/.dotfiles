a4ecaafd1294e5f7c5a24d1584806267298fd2fe
FateNozomi <wailinnyu@gmail.com>
2025-04-25 22:28:34 +0800

    Check if path exist before setting symbolic link

--- bash/install.sh
+++ bash/install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # neovim
+if [ ! -d ~/.config/nvim ]; then
 ln -s ~/.src/.dotfiles/nvim ~/.config/nvim
+fi
 
 # tmux
+if [ ! -d ~/.config/tmux ]; then
 ln -s ~/.src/.dotfiles/tmux ~/.config/tmux
+fi
 
 # zsh
 ln -s ~/.src/.dotfiles/zsh/.zshrc ~/.zshrc
