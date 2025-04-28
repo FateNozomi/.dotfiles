f8b12845701b77bbbd5f1e8d4e2228d801ff5df8
FateNozomi <wailinnyu@gmail.com>
2025-04-28 13:58:53 +1000

    Fix nvim roslyn cmd path

--- nvim/lua/plugins/lsp/dotnet.lua
+++ nvim/lua/plugins/lsp/dotnet.lua
@@ -4,6 +4,16 @@ return {
     ft = "cs",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
-    opts = {},
+    opts = {
+      config = {
+        cmd = {
+          "dotnet",
+          "/opt/roslyn/Microsoft.CodeAnalysis.LanguageServer.dll",
+          "--logLevel=Information",
+          "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+          "--stdio",
+        },
+      },
+    },
   },
 }
