195d1a7bf23fd00f778842da8c39a738102f2e4d
William Liew <william.liew@belldirect.com.au>
2025-05-29 11:49:31 +1000

    Update nvim dotnet roslyn lsp config

--- nvim/lua/plugins/lsp/dotnet.lua
+++ nvim/lua/plugins/lsp/dotnet.lua
@@ -4,16 +4,6 @@ return {
     ft = "cs",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
-    opts = {
-      config = {
-        cmd = {
-          "dotnet",
-          "/opt/roslyn/Microsoft.CodeAnalysis.LanguageServer.dll",
-          "--logLevel=Information",
-          "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-          "--stdio",
-        },
-      },
-    },
+    opts = {},
   },
 }
