95788c3de31f920bc53d8a661847e761ecb35e97
FateNozomi <wailinnyu@gmail.com>
2025-08-01 15:51:27 +1000

    Switch nvim neotest dotnet to vstest

--- nvim/lua/plugins/test/cs.lua
+++ nvim/lua/plugins/test/cs.lua
@@ -3,23 +3,11 @@ return {
     "nvim-neotest/neotest",
     optional = true,
     dependencies = {
-      { "fatenozomi/neotest-dotnet", branch = "dev" },
+      { "nsidorenco/neotest-vstest" },
     },
     opts = {
       adapters = {
-        ["neotest-dotnet"] = {
-          dap = {
-            args = { justMyCode = false },
-            adapter_name = "coreclr",
-          },
-          custom_attributes = {
-            mstest = { "DynamicDataAttribute" },
-          },
-          dotnet_additional_args = {
-            "--verbosity detailed",
-          },
-          discovery_root = "project",
-        },
+        ["neotest-vstest"] = {},
       },
     },
   },
