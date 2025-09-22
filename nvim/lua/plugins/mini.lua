64f6703bc785fb2b1b6200db0aaa625a4891f3f7
FateNozomi <wailinnyu@gmail.com>
2025-09-22 12:25:27 +1000

    Switch nvim which-key to mini.clue

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,5 +1,5 @@
 -- Centered on screen
-local win_config = function()
+local minipick_win_config = function()
   local height = math.floor(0.7 * vim.o.lines)
   local width = math.floor(0.8 * vim.o.columns)
   return {
@@ -20,13 +20,68 @@ return {
       require("mini.pairs").setup()
       require("mini.surround").setup()
       -- worlkflow
+      local miniclue = require("mini.clue")
+      miniclue.setup({
+        triggers = {
+          -- Leader triggers
+          { mode = "n", keys = "<Leader>" },
+          { mode = "x", keys = "<Leader>" },
+
+          -- Built-in completion
+          { mode = "i", keys = "<C-x>" },
+
+          -- `g` key
+          { mode = "n", keys = "g" },
+          { mode = "x", keys = "g" },
+
+          -- Marks
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
+
+          -- Registers
+          { mode = "n", keys = '"' },
+          { mode = "x", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "c", keys = "<C-r>" },
+
+          -- Window commands
+          { mode = "n", keys = "<C-w>" },
+
+          -- `z` key
+          { mode = "n", keys = "z" },
+          { mode = "x", keys = "z" },
+
+          -- `[` and `]` key
+          { mode = "n", keys = "[" },
+          { mode = "x", keys = "[" },
+          { mode = "n", keys = "]" },
+          { mode = "x", keys = "]" },
+        },
+        clues = {
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+        window = {
+          config = { width = "auto" },
+          delay = 200,
+          -- Keys to scroll inside the clue window
+          scroll_down = "<C-d>",
+          scroll_up = "<C-u>",
+        },
+      })
       require("mini.files").setup({ windows = { preview = true } })
       require("mini.extra").setup()
       require("mini.pick").setup({
         mappings = {
           choose_marked = "<C-q>",
         },
-        window = { config = win_config },
+        window = { config = minipick_win_config },
       })
       -- appearance
       require("mini.indentscope").setup()
