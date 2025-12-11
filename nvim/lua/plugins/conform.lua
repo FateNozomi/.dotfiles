d28f511cc4668ea2f293fd8d652f51bdd5fde08b
FateNozomi <wailinnyu@gmail.com>
2025-12-11 19:39:20 +1100

    Add nvim conform format html

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
         typescript = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         yaml = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
       },
       format_on_save = function(bufnr)
         -- Disable with a global or buffer-local variable
