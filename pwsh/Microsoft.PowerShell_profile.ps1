b2c99204053f87558f2192506b95dbd41c8054d9
FateNozomi <wailinnyu@gmail.com>
2025-12-18 08:39:35 +0800

    Set pwsh Invoke-Starship-PreCommand to tell CWD

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,15 +4,26 @@ Set-Alias -Name lg -Value lazygit.exe
 Set-Alias -Name nvd -Value neovide
 
 function hc {
-    Clear-Content (Get-PSReadlineOption).HistorySavePath
+    Clear-Content (Get-PSReadLineOption).HistorySavePath
     Clear-History
 }
+
+function Invoke-Starship-PreCommand {
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+    $prompt = "$([char]27)]9;12$([char]7)"
+    if ($loc.Provider.Name -eq "FileSystem") {
+        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+    $host.ui.Write($prompt)
+}
+
 function zcd {
     $fzPath = $(fzf)
     if ($fzPath -ne $null) {
         Set-Location (Get-Item $fzPath).Directory.FullName
     }
 }
+
 function y {
     $tmp = (New-TemporaryFile).FullName
     yazi $args --cwd-file="$tmp"
@@ -22,15 +33,16 @@ function y {
     }
     Remove-Item -Path $tmp
 }
+
 function mpva {
     param (
         [string]$Link
     )
     mpv `
-    --ytdl-format='ba' `
-    --ytdl-raw-options="yes-playlist=" `
-    --term-playing-msg='Title: ${media-title}' `
-    $Link
+        --ytdl-format='ba' `
+        --ytdl-raw-options="yes-playlist=" `
+        --term-playing-msg='Title: ${media-title}' `
+        $Link
 }
 
 $env:GIT_SSH = "$env:SystemRoot\System32\OpenSSH\ssh.exe"
