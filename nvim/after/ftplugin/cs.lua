981bad92e90d2c960e1ef548be08cb62ab55da6f
FateNozomi <wailinnyu@gmail.com>
2025-12-23 16:24:16 +1100

    Set nvim cs filetype shiftwidth and tabstop to 4

--- nvim/after/ftplugin/cs.lua
+++ nvim/after/ftplugin/cs.lua
@@ -1,7 +1,10 @@
--- Fold with tree-sitter
-vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.wo[0][0].foldmethod = "expr"
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
 
 vim.g.dotnet_errors_only = true
 vim.g.dotnet_show_project_file = false
 vim.cmd("compiler dotnet")
+
+-- Fold with tree-sitter
+vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo[0][0].foldmethod = "expr"
