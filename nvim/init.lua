272012deae469f0431967b887aa0a808f6700d69
FateNozomi <wailinnyu@gmail.com>
2026-06-17 13:30:47 +1000

    fix: move  definition before first

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,14 @@
 -- Define config table to be able to pass data between scripts
 _G.Config = {}
 
-vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
-
-local misc = require("mini.misc")
-Config.now = function(f) misc.safely("now", f) end
-Config.later = function(f) misc.safely("later", f) end
-Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
-Config.on_event = function(ev, f) misc.safely("event:" .. ev, f) end
-Config.on_filetype = function(ft, f) misc.safely("filetype:" .. ft, f) end
-
 -- Define custom autocommand group and helper to create an autocommand.
+-- Autocommands are Neovim's way to define actions that are executed on events
+-- (like creating a buffer, setting an option, etc.).
+--
+-- See also:
+-- - `:h autocommand`
+-- - `:h nvim_create_augroup()`
+-- - `:h nvim_create_autocmd()`
 local gr = vim.api.nvim_create_augroup("custom-config", {})
 Config.new_autocmd = function(event, pattern, callback, desc)
   local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
@@ -32,6 +30,15 @@ Config.on_packchanged = function(plugin_name, kinds, callback, desc)
   end
   Config.new_autocmd("PackChanged", "*", f, desc)
 end
+
+vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
+
+local misc = require("mini.misc")
+Config.now = function(f) misc.safely("now", f) end
+Config.later = function(f) misc.safely("later", f) end
+Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
+Config.on_event = function(ev, f) misc.safely("event:" .. ev, f) end
+Config.on_filetype = function(ft, f) misc.safely("filetype:" .. ft, f) end
 
 require("options")
 require("keymaps")
